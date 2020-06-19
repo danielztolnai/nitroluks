d2ccb0a9a34f483b2470699a41a3482529c7d589
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2020-06-19 16:49:18 +0200

    Refactor: Move nitrokey to class

--- src/nitro_luks.cpp
+++ src/nitro_luks.cpp
@@ -1,126 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <inttypes.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
 #include <termios.h>
 #include <unistd.h>
-#include <libnitrokey/NK_C_API.h>
+#include "nitrokey.h"
 
-// return status codes
-#define ERROR 1
-#define RETRY_PASSWORD 2
-#define PASSWORD_OK 0
+// Return status codes
+#define RET_ERROR (-1)
+#define RET_OK (0)
 
-// libnitrokey status codes
-#define STATUS_OK 0
-#define NITROKEY_FOUND 1
-
-// parameters
-#define SLOT_COUNT 16
+// Parameters
 #define MAX_PIN_LENGTH 20
-#define NITROKEY_WAIT_TIMEOUT_MS 5000
-#define NITROKEY_WAIT_RETRY_INTERVAL_MS 250
-#define NITROKEY_MAX_RETRY_COUNT (NITROKEY_WAIT_TIMEOUT_MS / NITROKEY_WAIT_RETRY_INTERVAL_MS)
 
-struct termios saved_attributes;
+// Function prototypes
+std::string read_password();
 
-int error(char const *msg)
+// Entry point
+int main(int argc, char const *argv[])
 {
-    fprintf(stderr, "%s \n*** Falling back to default LUKS password entry.\n", msg);
-    return ERROR;
+    Nitrokey nitrokey{};
+    std::string slot_name{"LUKS"};
+
+    // Read password from stdin and remove the trailing newline
+    auto password = read_password();
+
+    if (0 == password.length()) {
+        return RET_ERROR;
+    }
+
+    if(!nitrokey.is_present()) {
+        return RET_ERROR;
+    }
+
+    nitrokey.unlock_safe(password);
+    if (!nitrokey.is_unlocked()) {
+        return RET_ERROR;
+    }
+
+    auto slot_password = nitrokey.find_slot_content(slot_name);
+    if (0 == slot_password.length()) {
+        return RET_ERROR;
+    }
+
+    std::cout << slot_password;
+    return RET_OK;
 }
 
-void disable_echo(void)
+std::string read_password()
 {
+    struct termios saved_attributes;
     struct termios tattr;
-    tcgetattr(STDIN_FILENO, &saved_attributes);
 
+    // Disable echo
+    tcgetattr(STDIN_FILENO, &saved_attributes);
     tcgetattr (STDIN_FILENO, &tattr);
     tattr.c_lflag &= ~(ICANON | ECHO);
     tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
-}
 
-void reset_input_mode(void)
-{
-    tcsetattr(STDIN_FILENO, TCSANOW, &saved_attributes);
-}
-
-int main(int argc, char const *argv[])
-{
-    // Disable debug messages
-    NK_set_debug(false);
-
-    // Check for nitrokey
-    int login_status = !NITROKEY_FOUND;
-    for (int retry = 0; retry < NITROKEY_MAX_RETRY_COUNT; ++retry) {
-        login_status = NK_login_auto();
-        if (login_status == NITROKEY_FOUND) {
-            const char* serial = NK_device_serial_number();
-            fprintf(stderr, "*** Nitrokey : %s found!\n", serial);
-            break;
-        }
-        usleep(NITROKEY_WAIT_RETRY_INTERVAL_MS*1000);
-    }
-    if (login_status != NITROKEY_FOUND) {
-        return error("*** No nitrokey detected.");
-    }
-
-    // Check if user pin is locked
-    uint8_t retry_count = NK_get_user_retry_count();
-    if (retry_count == 0) {
-        return error("*** User PIN locked.");
-    }
-
-    // Read password from stdin and remove the trailing newline
+    // Read password and remove trailing new line
     char password[MAX_PIN_LENGTH + 1] = {'\0'};
-    disable_echo();
     fgets(password, sizeof(password), stdin);
-    reset_input_mode();
     password[strcspn(password, "\n")] = 0;
 
-    // Check password length
-    if (strlen(password) == 0) {
-        return error("*** Empty password, falling back to LUKS passphrase");
-    }
+    // Reset stdin
+    tcsetattr(STDIN_FILENO, TCSANOW, &saved_attributes);
 
-    // Unlock password safe
-    int password_safe_status = NK_enable_password_safe(password);
-    if (password_safe_status != STATUS_OK) {
-        fprintf(stderr, "*** Error while accessing password safe.\n");
-        return RETRY_PASSWORD;
-    }
-
-    // Find LUKS password
-    int empty_slots = 0;
-    uint8_t slot_number = 255;
-    fprintf(stderr, "*** Scanning the nitrokey slots...\n");
-    uint8_t *slots = NK_get_password_safe_slot_status();
-    for (uint8_t slot = 0; slot < SLOT_COUNT; ++slot) {
-        if (slots[slot] == 1) {
-            const char* slotname = NK_get_password_safe_slot_name(slot);
-            if(strcmp(slotname, "LUKS") == 0) {
-                slot_number = slot;
-            }
-        } else {
-            empty_slots++;
-        }
-    }
-
-    if (empty_slots == SLOT_COUNT) {
-        return error("*** No slots enabled.");
-    }
-    if(slot_number == 255) {
-        return error("*** No slot configured by name LUKS.");
-    }
-
-    // Read the LUKS password and print it to stdout
-    const char* LUKS_password = NK_get_password_safe_slot_password(slot_number);
-    fprintf(stdout, "%s", LUKS_password);
-
-    // Close the device and disconnect
-    NK_lock_device();
-    NK_logout();
-
-    return PASSWORD_OK;
+    return std::string(password);
 }
---
+++ src/nitrokey.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <libnitrokey/NK_C_API.h>
+
+class Nitrokey
+{
+private:
+    const std::size_t slot_count = 16;
+    bool present = false;
+    bool unlocked = false;
+
+    bool login() { return (1 == NK_login_auto()); }
+    void logout() { NK_logout(); }
+    bool unlock(const char* password) { return (0 == NK_enable_password_safe(password)); }
+    void lock() { NK_lock_device(); }
+    void set_debug(bool value) { NK_set_debug(value); }
+    bool is_user_pin_locked() { return (0 == NK_get_user_retry_count()); }
+
+    const std::vector<uint8_t> get_slot_status() {
+        uint8_t *slots_ptr = NK_get_password_safe_slot_status();
+        std::vector<uint8_t> slots;
+        for (uint8_t slot = 0; slot < this->slot_count; ++slot) {
+            slots.push_back(slots_ptr[slot]);
+        }
+        delete[] slots_ptr;
+        return slots;
+    }
+
+    std::string get_slot_name(uint8_t slot_number) {
+        const char* name_ptr = NK_get_password_safe_slot_name(slot_number);
+        std::string name(name_ptr);
+        if (name_ptr) {
+            free(const_cast<char*>(name_ptr));
+        }
+        return name;
+    }
+
+    std::string get_slot_password(uint8_t slot_number) {
+        const char* password_ptr = NK_get_password_safe_slot_password(slot_number);
+        std::string password{password_ptr};
+        if (password_ptr) {
+            free(const_cast<char*>(password_ptr));
+        }
+        return password;
+    }
+
+    void detect(std::size_t detect_timeout_ms, std::size_t detect_interval_ms) {
+        std::size_t detect_retry_count = detect_timeout_ms / detect_interval_ms;
+
+        for (std::size_t retry = 0; retry < detect_retry_count; ++retry) {
+            if (this->login()) {
+                std::cerr << "[INFO] Nitrokey found" << std::endl;
+                this->present = is_user_pin_locked() ? false : true;
+                return;
+            }
+            usleep(detect_interval_ms * 1000);
+        }
+
+        this->present = false;
+        std::cerr << "[INFO] Nitrokey not found" << std::endl;
+    }
+
+public:
+    Nitrokey(std::size_t detect_timeout_ms = 5000, std::size_t detect_interval_ms = 250) {
+        this->set_debug(false);
+        this->detect(detect_timeout_ms, detect_interval_ms);
+    }
+
+    ~Nitrokey() {
+        this->lock();
+        this->logout();
+        std::cerr << "[INFO] Nitrokey locked" << std::endl;
+    }
+
+    bool is_present() { return this->present; }
+    bool is_unlocked() { return this->unlocked; }
+
+    void unlock_safe(std::string password) {
+        this->unlocked = this->unlock(password.c_str());
+        std::cerr << "[INFO] Nitrokey unlock " << (this->unlocked ? "successful" : "failed") << std::endl;
+    }
+
+    std::string find_slot_content(std::string const slot_name) {
+        if (!this->unlocked) {
+            return "";
+        }
+
+        auto slots = this->get_slot_status();
+        for (std::size_t slot = 0; slot < slots.size(); ++slot) {
+            if (slots[slot] == 1) {
+                if(slot_name == this->get_slot_name(slot)) {
+                    return this->get_slot_password(slot);
+                }
+            }
+        }
+
+        std::cerr << "[INFO] Slot named '" << slot_name << "' not found" << std::endl;
+        return "";
+    }
+};
