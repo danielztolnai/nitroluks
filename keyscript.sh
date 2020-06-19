b3b04916ef43828a3ea49d00211e1207cfaa3fbd
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2020-06-19 17:56:36 +0200

    Do not retry internally, use the crypttab retry method

--- keyscript.sh
+++ keyscript.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
-echo "*** Trying to unlock the disk $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) by nitrokey, waiting for the USB to be recognized..." >&2
+echo "[keyscript] Unlocking $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) by nitrokey, waiting for device..." >&2
 
-# set exit status to fail so we fall back to default luks prompt if execution of nitro_luks does not work for some reason
+# Try to read the password from Nitrokey
 exit_status=1
-retry_pin=2
-
-# call nitroluks to get the LUKS key
-while [ $retry_pin -eq 2 ]; do
-    /lib/cryptsetup/askpass "Please unlock nitrokey: " | /bin/nitro_luks
+if /bin/nitro_luks -d; then
+    /lib/cryptsetup/askpass "* Please unlock disk ${CRYPTTAB_NAME}: " | /bin/nitro_luks
     exit_status=$?
-    retry_pin=$exit_status
-done
+fi
 
-# in case nitroluks fails, fall back to the default LUKS password prompt
+# In case reading from Nitrokey fails, fall back to a LUKS password prompt
 if [ $exit_status -eq 1 ]; then
     /lib/cryptsetup/askpass "Please unlock disk ${CRYPTTAB_NAME}: "
-else
-    echo "*** LUKS setup successful by nitrokey" >&2
 fi
