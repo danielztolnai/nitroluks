501d1e12a408b0f203310ea4dceebd54f7817e77
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2020-06-19 09:56:50 +0200

    Keyscript: Retry pin entry if unlock failed

--- keyscript.sh
+++ keyscript.sh
@@ -3,11 +3,14 @@ echo "*** Trying to unlock the disk $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) by nitroke
 
 # set exit status to fail so we fall back to default luks prompt if execution of nitro_luks does not work for some reason
 exit_status=1
+retry_pin=2
 
 # call nitroluks to get the LUKS key
-/bin/nitro_luks
-
-exit_status=$?
+while [ $retry_pin -eq 2 ]; do
+    /lib/cryptsetup/askpass "Please unlock nitrokey: " | /bin/nitro_luks
+    exit_status=$?
+    retry_pin=$exit_status
+done
 
 # in case nitroluks fails, fall back to the default LUKS password prompt
 if [ $exit_status -eq 1 ]; then
