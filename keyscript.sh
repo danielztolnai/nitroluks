66efb3843dda612e8bd821d35d8789de9ba875be
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2020-06-19 18:12:32 +0200

    Add custom slot support to the keyscript

--- keyscript.sh
+++ keyscript.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 echo "[keyscript] Unlocking $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) by nitrokey, waiting for device..." >&2
 
+# Check for custom slot name
+SLOT_NAME=""
+if [ "none" != "${CRYPTTAB_KEY}" ]; then
+    SLOT_NAME="${CRYPTTAB_KEY}"
+fi
+
 # Try to read the password from Nitrokey
 exit_status=1
 if /bin/nitro_luks -d; then
-    /lib/cryptsetup/askpass "* Please unlock disk ${CRYPTTAB_NAME}: " | /bin/nitro_luks
+    /lib/cryptsetup/askpass "* Please unlock disk ${CRYPTTAB_NAME}: " | /bin/nitro_luks "${SLOT_NAME}"
     exit_status=$?
 fi
 
